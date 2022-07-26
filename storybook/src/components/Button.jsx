743544818c7a38f31b2ef12367372d4f7bf22e19
Avzaradel Daniel <E038370@esl.corp.elbit.co.il>
2022-07-26 17:14:22 +0300

    removed import react

--- storybook/src/components/Button.jsx
+++ storybook/src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Button = ({ label, bgColor, primary}) => {
     return(
         <button style={{background: bgColor, color: primary ? 'white' : 'black'}}>{label}</button>
